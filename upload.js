5bb86200793258afc62996942c40efdeca71a6bf
rramchandani12 <128962350+rramchandani12@users.noreply.github.com>
2023-03-26 19:11:15 +1100

    Add files via upload

---
+++ upload.js
@@ -0,0 +1,97 @@
+// Client ID and API key from the Developer Console
+var CLIENT_ID = '362278784795-i4lk4b8m8ftqo7q1pcnus13oq2e5ns2s.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyCQaiNm9n4cMjyTD5dj_gW1NSnP75j_Zzo';
+
+// Array of API discovery doc URLs for APIs used by the quickstart
+var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+var SCOPES = 'https://www.googleapis.com/auth/drive.file';
+
+var authorizeButton = document.getElementById('authorize_button');
+var signoutButton = document.getElementById('signout_button');
+
+/**
+ *  On load, called to load the auth2 library and API client library.
+ */
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
+}
+
+/**
+ *  Initializes the API client library and sets up sign-in state
+ *  listeners.
+ */
+function initClient() {
+  gapi.client.init({
+    apiKey: API_KEY,
+    clientId: CLIENT_ID,
+    discoveryDocs: DISCOVERY_DOCS,
+    scope: SCOPES
+  }).then(function () {
+    // Listen for sign-in state changes.
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+    // Handle the initial sign-in state.
+    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    authorizeButton.onclick = handleAuthClick;
+    signoutButton.onclick = handleSignoutClick;
+  }, function(error) {
+    appendPre(JSON.stringify(error, null, 2));
+  });
+}
+
+/**
+ *  Called when the signed in status changes, to update the UI
+ *  appropriately. After a sign-in, the API is called.
+ */
+function updateSigninStatus(isSignedIn) {
+  if (isSignedIn) {
+    authorizeButton.style.display = 'none';
+    signoutButton.style.display = 'block';
+  } else {
+    authorizeButton.style.display = 'block';
+    signoutButton.style.display = 'none';
+  }
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick(event) {
+  gapi.auth2.getAuthInstance().signIn();
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick(event) {
+  gapi.auth2.getAuthInstance().signOut();
+}
+
+/**
+ * Uploads a file to the signed-in user's Google Drive.
+ */
+function uploadFile() {
+  var file = document.getElementById('file').files[0];
+  var metadata = {
+    'name': file.name,
+    'parents': ['<FOLDER_ID>'] // Replace <FOLDER_ID> with the ID of the folder you want to upload to.
+  };
+  var accessToken = gapi.auth.getToken().access_token;
+  var form = new FormData();
+  form.append('metadata', new Blob([JSON.stringify(metadata)], {type: 'application/json'}));
+  form.append('file', file);
+
+  fetch('https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&fields=id', {
+    method: 'POST',
+    headers: new Headers({'Authorization': 'Bearer ' + accessToken}),
+    body: form,
+  }).then((res) => {
+    return res.json();
+  }).then(function(val) {
+    console.log('File Id: ' + val.id);
+    alert('File uploaded successfully to Google Drive!');
+  });
+}
